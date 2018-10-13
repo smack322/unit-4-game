49473210526336a4ff5e00600f6e3272617e38a9
Scott McAnally <scottmcanally@Scotts-MacBook-Pro.local>
2018-10-13 15:25:45 -0400

    added logic and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,79 @@
-//initialize variables to store wins, losses, total score
+//initialize variables to store wins, losses
 var winsCounter = 0;
 var lossesCounter = 0;
-var totalScore = 0;
+
 
 //initialize unique random numbers for images & random number to try and get to
 
 var a = [1,2,3,4,5,6,7,8,9,10,11,12];
-var n;
-var r=[];
-for (n=1; n<=4; ++n)
-{
-  var i = Math.floor((Math.random() * (12-n)) + 1);
-  r.push(a[i]);
-  a[i] = a[12-n];
-}
-var randomStartingNum = Math.floor((Math.random() * 120) + 19);
-var diamondValue = r[0];
-var emeraldValue = r[1]
-var rubyValue = r[2];
-var saphireValue = r[3];
+var randomStartingNum;
+var diamondValue;
+var emeraldValue;
+var rubyValue;
+var saphireValue;
+var totalScore;
 
-console.log(randomStartingNum);
-console.log(diamondValue);
-console.log(emeraldValue);
-console.log(rubyValue);
-console.log(saphireValue);
+// store unique random numbers in each gem (between 1 - 12) and a large random number between 19 - 120
+
+function getRandomValues () {
+    var r=[];
+    for (var n=1; n<=4; ++n)
+    {
+      var i = Math.floor((Math.random() * (12-n)) + 1);
+      r.push(a[i]);
+      a[i] = a[12-n];
+    }
+     randomStartingNum = Math.floor((Math.random() * 120) + 19);
+     $("#randomNumBox").text(randomStartingNum);
+     diamondValue = r[0];
+     emeraldValue = r[1]
+     rubyValue = r[2];
+     saphireValue = r[3];
+     totalScore = 0;
+     $("#elementTotalNum").text("Your total score is:");
+     $("#elementNum").text(totalScore);
+}
+
 
 // function to reset the game
 
-function resetGame () {
+function checkWin () {
     if (totalScore === randomStartingNum) {
        winsCounter++;
-       totalScore = 0;
-       randomStartingNum;
-       diamondValue;
-       emeraldValue;
-       rubyValue;
-       saphireValue;
-       winsText; //needs to be used with the DOM   
+       $("#gameWon").text("Wins: " + winsCounter);
+       getRandomValues();     
     }
-    else {
+    else if (totalScore > randomStartingNum) {
         lossesCounter++;
-        totalScore = 0;
-        randomStartingNum;
-        diamondValue;
-        emeraldValue;
-        rubyValue;
-        saphireValue;
-        lossText; //needs to be used with the DOM
+        $("#gameLost").text("Losses: " + lossesCounter);
+        getRandomValues();    
     }
 }
 
-// select and write javascript variables to html
-var winsText = $("#winsLosses");
-var newWinP = $("<p>" + winsText + "</p>");
-winsText.text(winsCounter);
-var lossesText = $("#winsLosses");
-var newLossP = $("<p>" + lossText + "</p>");
-lossesText.text(lossesCounter);
-
-// initialize variables that store display message on the win or loss
-var winsText = "You Won!!!";
-var lossText = "You lost!!!";
-
 // when the user clicks a gem the total score is adjusted based on the value of the diamond
 $('#diamond').on("click", function () {
-   alert("I am a diamond");
     totalScore += diamondValue;
+    $("#elementNum").text(totalScore);
+    checkWin();
+    console.log(totalScore);
 });
 
 $('#emerald').on("click", function () {
-    alert("I am an emerald");
     totalScore += emeraldValue;
+    $("#elementNum").text(totalScore);
+    checkWin();
  });
 
  $('#ruby').on("click", function () {
-    alert("I am a ruby");
     totalScore += rubyValue;
+    $("#elementNum").text(totalScore);
+    checkWin();
  });
 
  $('#saphire').on("click", function () {
-    alert("I am a saphire");
     totalScore += saphireValue;
+    $("#elementNum").text(totalScore);
+    checkWin();
  });
+
+ getRandomValues();
